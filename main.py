14e9d2afd008aa7a02b7a4d2603d7fff539d61ce
Nec-ro <mrnecrobrawl@gmail.com>
2026-06-05 20:22:21 +0330

    added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+class UniversitySystem:
+    def __init__(self):
+        self.courses = {"Math 1": 3, "Physics 1": 3, "Programming": 4, "Data Structures": 4}
+        self.passed_courses = []
+
+    def calculate_gpa(self, grades):
+        # TODO
+        pass
+
+    def can_enroll(self, course_name, pre_requisites_met):
+        # TODO
+        pass
